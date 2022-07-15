0e7f25e7a07c82c344817b4844f1c957cddc3765
Dzhabrail <dzhabulya@gmail.com>
2022-07-15 04:09:14 +0300

    Added filetypes

--- lua/browse/devdocs.lua
+++ lua/browse/devdocs.lua
@@ -2,13 +2,24 @@ local utils = require("browse.utils")
 
 local M = {}
 
+local filetypes_mapping = {
+  scss = 'css',
+  sass = 'css',
+  javascriptreact = 'javascript',
+  typescriptreact = 'javascript',
+  typescript = 'javascript',
+}
+
 M.search_with_filetype = function()
   vim.ui.input("Search String: ", function(input)
     if input == nil or input == "" then
       return
     end
     local open_cmd = utils.get_open_cmd()
-    local input_with_filetype = vim.bo.filetype .. " " .. input
+    local filetype = vim.bo.filetype;
+    filetype = filetypes_mapping[filetype] or filetype;
+
+    local input_with_filetype = filetype .. " " .. input
     local url = string.format('https://devdocs.io/#q=%s', input_with_filetype)
     if utils.is_mac_os() then
       url = utils.urlencode(url)
